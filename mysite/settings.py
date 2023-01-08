19fbb5a4c4f1308c840d408dca4df660f93ecb81
Renato <varejao1-mail@yahoo.com.br>
2023-01-08 12:22:24 -0300

    Corrigindo allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jm$_b9ypvwu-!&d8t-co)@ecocuqhzhvpt4b8t+i*rw0$)pomo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
